3acd2b4933a103317bddc8bb4e172c0a1139c0e4
stefanoviel <stefi.viel@gmail.com>
2025-02-26 21:39:55 +0100

    let's encrypt certs

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,5 +3,22 @@
 # Replace API_URL in all HTML files
 find /usr/share/nginx/html -type f -name "*.html" -exec sed -i "s|window.API_URL|'${API_URL}'|g" {} \;
 
+# Your domain name
+DOMAIN="myparking.online/"
+
+# Check if certificates already exist
+if [ ! -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem ]; then
+    # Get certificates for the first time
+    certbot --nginx \
+            --non-interactive \
+            --agree-tos \
+            --email viel.stefano01@gmail.com \
+            -d $DOMAIN \
+            --redirect
+
+    # Set up auto-renewal
+    (crontab -l 2>/dev/null; echo "0 12 * * * /usr/bin/certbot renew --quiet") | crontab -
+fi
+
 # Start nginx
 exec "$@" 
